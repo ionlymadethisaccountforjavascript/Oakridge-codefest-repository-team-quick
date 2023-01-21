ae76ed07a92a81bcf4127790aefbc40676238397
ionlymadethisaccountforjavascript <108260879+ionlymadethisaccountforjavascript@users.noreply.github.com>
2023-01-21 22:11:08 +0530

    More elements added to original file
    The command section for the Create account button is blank.

--- Registration.py
+++ Registration.py
@@ -38,6 +38,40 @@ Label(master, text = "Enter your Aadhar card pin").grid(row = 7)
 e3 = Entry(master)
 e3.grid(row = 7, column = 1)
 
+#Password generator
+def PasswordGenerator():
+    import random
+
+    x = str(random.randint(100,999))
+    y = random.randint(0,1)
+    a = random.randint(0,25)
+    b = random.randint(0,1)
+    c = random.randint(0,25)
+    d = random.randint(0,1)
+    z = random.randint(0,25)
+
+    upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    letters = [upperCase,lowerCase]
+    def password():
+        hop = letters[y][z]
+        hop1 = letters[b][a]
+        hop2 = letters[d][c]
+        final=x+hop+hop1+hop2
+
+        return final
+
+
+    xx=password()
+    Label(master, text = xx).grid(row = 10)
+
+Label(master, text = "Unique ID generator").grid(row = 8)
+Label(master, text = "").grid(row = 8)
+Button(master, text = "Generate", command = PasswordGenerator).grid(row =9)
+
+Button(master, text ="Create account", command = "" ).grid(row = 30, column= 1)
+
+
 mainloop()
 
 
