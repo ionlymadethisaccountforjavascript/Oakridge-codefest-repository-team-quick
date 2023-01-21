5f6da53f3733e2db983e731c3e776cd7c125c618
ionlymadethisaccountforjavascript <108260879+ionlymadethisaccountforjavascript@users.noreply.github.com>
2023-01-22 05:17:46 +0530

    Add files via upload

--- Registration.py
+++ Registration.py
@@ -3,8 +3,6 @@ import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
 from tkinter.filedialog import askopenfile
-from PIL import ImageTk, Image
-
 
 gui = Tk(className='quickDoc')
 gui.geometry("400x200")
@@ -23,14 +21,14 @@ def createAccountPage():
     master = Tk() # initialization
     master.title("Registration")
     master.geometry("400x400") # size of window
-    Label(master, text = "First name").grid(row = 0, column = 0) #positioning of first column text
-    Label(master, text = "Last name").grid(row = 1, column = 0)
+    Label(master, text = "First name").grid(row = 0, column = 0) #positioning of first column 
     e1 = Entry(master)
+    Label(master, text = "Last name").grid(row = 1, column = 0)
     e2 = Entry(master)
 
     e1.grid(row = 0, column = 1)
     e2.grid(row = 1, column = 1)
-
+    
     #subheading
     Label(master, text = "Gender select").grid(row = 2, column = 0)
     #checkbox part
@@ -90,6 +88,8 @@ def createAccountPage():
     e3 = Entry(master)
     e3.grid(row = 7, column = 1)
 
+    e7 = e3.get()
+
     #Password generator
     def PasswordGenerator():
         import random
@@ -116,7 +116,7 @@ def createAccountPage():
 
         xx=password()
         Label(master, text = xx).grid(row = 10)
-
+    pg = PasswordGenerator()
     Label(master, text = "Unique ID generator").grid(row = 8)
     Label(master, text = "").grid(row = 8)
     Button(master, text = "Generate", command = PasswordGenerator).grid(row =9)
@@ -128,27 +128,183 @@ def createAccountPage():
         main.geometry("400x400") # size of window
         
         def upload_files():
-            main.destroy()
             upld_fles = Tk()# initialization of upload files section
             upld_fles.title("Upload files")
             upld_fles.geometry("400x400") # size of window
             def upld_fls_pg():
-                upld_fles.destroy()
                 upld_fls = Tk()# initialization of upload files section
                 upld_fls.title("Upload files")
                 upld_fls.geometry("400x400") # size of window
-            img = Button(upld_fles, text = "Upload your files here", command = askopenfile()).grid(row = 9, column = 1)
-            #Frame for image display
-            frame = Frame(upld_fles, width = 800, height =600)
-            frame.grid(row=2, column=1)
-            frame.place(anchor = 'center', relx= 0.5, rely=0.5)
-            img1 = ImageTk.PhotoImage(Image.open("Prisoner.png"))
-            label = Label(frame, image=img1, )
-            label.pack()
+            Button(upld_fles, text = "Upload your files here", command = askopenfile()).grid(row = 0, column = 1)
+
+        Button(main, text = "upload files", command = upload_files).grid(row =1)
+        def account_page():
+            accpage = Tk()# initialization of account page
+            accpage.title("Account")
+            accpage.geometry("400x400") # size of window
+            accpage.title("Account details")
+            def acc():
+                accprn1 = Tk()
+                accprn1.title("Account details")
+                accprn1.geometry("400x400") # size of window
+                Label(e1)
+            Button(accpage, text = "name", command = acc)
+
+            lbl.config(text = "First Name: "+e5)
+            e5 = tk.Text(accpage, height = 5, width = 20)
+            e5.pack()
+            lbl = tk.Label(accpage, text = "")
+            lbl.pack()
+            e7 = acc()
+            def acc1():
+                e6 = e2.get()
+                lbl.config(text = "Last Name: "+e6)
+                e6 = tk.Text(accpage, height = 5, width = 20)
+                e6.pack()
+                lbl = tk.Label(accpage, text = "")
+                lbl.pack()
+            e8 = acc1()
+            #Account details
+            Label(accpage, text=e7).grid(row = 2, column = 1)
+            Label(accpage, text=e8).grid(row = 3, column = 1)
+            #Aadhar pin number
+            Label(accpage, text = "Aadhar card pin",).grid(row = 5, column = 1)
+            Button(accpage,text = "Obtain Pin", command = PasswordGenerator).grid()
+            #Password
+            Label(accpage, text = "Password",).grid(row = 7, column = 1)
+            Label(accpage, text = pg,).grid(row = 8, column = 1)
         
-        upload_files = Button(main, text = "upload files", command = upload_files).grid(row =15)
+        Button(main, text = "Account details", command = account_page).grid( row = 1, column = 4)
+
+        def aiConsultant():
+            aiCons = Tk()# initialization of ai consultant page
+            aiCons.title("AI Consultant")
+            aiCons.geometry("400x400") # size of window
+            import speech_recognition as sr
+            import pyttsx3 as tts
+            import random as rdm    
+            import requests as req
+            import datetime as dt
+            import os
+            from googlesearch import search
+            import time as tm
+            from urllib.request import urlopen
+            import wikipedia as wk
+            import pyaudio
+            from os.path import exists
+            import tkinter as tk
+            import wikipediaapi as wkapi
+            import nltk
+            from nltk.tokenize import sent_tokenize, word_tokenize
+            from nltk.corpus import stopwords
+            from nltk.probability import FreqDist
+
+            wiki_wiki = wkapi.Wikipedia('en')
+
+    
+            engine = tts.init('sapi5')
+            voices = engine.getProperty('voices')
+            engine.setProperty('voice', voices[0].id)
+
+
+            def speak(audio):
+                engine.say(audio)   
+                engine.runAndWait()
+
+            def getQuery():
+                r = sr.Recognizer()
+                while True:
+                    with sr.Microphone(device_index=1) as source:
             
-        main.mainloop()
+                        speak("Please tell us your illness or condition... ")
+                        Label(aiCons,"Please tell us your illness or condition... ")
+                        audio=r.listen(source)
+                        Label(aiCons,audio)
+                        try:    
+                            queryspeak = r.recognize_google(audio)
+                            return queryspeak                   
+                            break
+                        except:
+                            Label(aiCons,text = "Try Again")
+
+    
+            speak("Quick    and    easy    medical    solutions    by    quick doc") #Started
+            ("Would you like to type or speak? (T/S)")
+            mode=input(" ")
+            while True:
+                if (mode=="T"):
+                    ("1. View details of your illness")
+                    ("2. View symptoms of your illness")
+                    ("3. View preventive measures for your illness")
+                    opt=int(input(" "))       
+                    if opt==1:
+                        qtext=input("Please enter your illness or condition: ")
+                        qtext=qtext.lower()
+                        ("\n")
+                        (wk.summary(qtext,5))
+                        speak(wk.summary(qtext,5)) 
+                    elif opt==2:
+                        qtext=input("Please enter your illness or condition: ")      
+                        qtext=qtext.lower()
+                        pagepy=wiki_wiki.page(qtext)
+                        symptoms = pagepy.section_by_title('Signs and symptoms') 
+                        stringstring=symptoms.full_text().split('\n')
+                        symp=' '.join(stringstring[1].split()[0:150])
+                        (symp)
+                        ("\n")
+                    elif opt==3:
+                        qtext=input("Please enter your illness or condition: ")
+                        pagepy=wiki_wiki.page(qtext)
+                        symptoms = pagepy.section_by_title('Prevention') 
+                        stringstring=symptoms.full_text().split('\n')
+                        symp=' '.join(stringstring[1].split()[0:150])
+                        (symp)
+                        ("\n")
+
+
+
+                    elif (mode=="S"):
+                        ("1. View details of your illness")
+                        speak("View details of your illness")
+                        ("2. View symptoms of your illness")
+                        speak("View symptoms of your illness")
+                        ("3. View preventive measures for your illness")
+                        speak("View preventive measures for your illness")
+                        tm.sleep(2)
+                        speak("What would you like to do?")
+                        opt=int(input(" "))       
+                    if opt==1:
+                        ("Please enter your illness or condition: ")
+                        speak("Please enter your illness or condition: ")
+                        qspeak=getQuery()
+                        ("\n")
+                        (wk.summary(qspeak,5))
+                        speak(wk.summary(qspeak,5)) 
+                    elif opt==2:
+                        ("Please enter your illness or condition: ") 
+                        speak("Please enter your illness or condition: ")
+                        qspeak=getQuery()    
+                        pagepy=wiki_wiki.page(qspeak)
+                        symptoms = pagepy.section_by_title('Signs and symptoms') 
+                        stringstring=symptoms.full_text().split('\n')
+                        symp=' '.join(stringstring[1].split()[0:150])
+                        (symp)
+                        speak(symp)
+                        ("\n")
+                    elif opt==3:
+                        ("Please enter your illness or condition: ")
+                        speak("Please enter your illness or condition: ")
+                        qspeak=getQuery()
+                        pagepy=wiki_wiki.page(qspeak)
+                        symptoms = pagepy.section_by_title('Prevention') 
+                        stringstring=symptoms.full_text().split('\n')
+                        symp=' '.join(stringstring[1].split()[0:150])
+                        (symp)
+                        speak(symp)
+                        ("\n")
+
+            
+        Button(main,text = "AI consultant", command = aiConsultant).grid(column=6, row = 1)
 
     home_page = tk.Button(master, text ="Create account", command = main_page ).grid(row = 30, column= 1)    
 
