dbc414c4814548848269a3dfad5ac2609ca31013
Daksh Aggarwal <74252852+DakshAggarwal0903@users.noreply.github.com>
2023-01-22 08:21:02 +0530

    Add files via upload

--- QuikDoc.py
+++ QuikDoc.py
@@ -1,129 +1,124 @@
-def aiAssistant():
-    import speech_recognition as sr
-    import pyttsx3 as tts
-    import random as rdm    
-    import requests as req
-    import datetime as dt
-    import os
-    from googlesearch import search
-    import time as tm
-    from urllib.request import urlopen
-    import wikipedia as wk
-    import pyaudio
-    from os.path import exists
-    import tkinter as tk
-    import wikipediaapi as wkapi
-    import nltk
-    from nltk.tokenize import sent_tokenize, word_tokenize
-    from nltk.corpus import stopwords
-    from nltk.probability import FreqDist
+import speech_recognition as sr
+import pyttsx3 as tts
+import random as rdm    
+import requests as req
+import datetime as dt
+import os
+from googlesearch import search
+import time as tm
+from urllib.request import urlopen
+import wikipedia as wk
+import pyaudio
+from os.path import exists
+import tkinter as tk
+import wikipediaapi as wkapi
 
-    wiki_wiki = wkapi.Wikipedia('en')
+wiki_wiki = wkapi.Wikipedia('en')
+engine = tts.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
 
 
-    engine = tts.init('sapi5')
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[0].id)
+   
 
+def speak(audio):
+    engine.say(audio)   
+    engine.runAndWait()
 
-    def speak(audio):
-        engine.say(audio)   
-        engine.runAndWait()
-
-    def getQuery():
-        r = sr.Recognizer()
-        while True:
-            with sr.Microphone(device_index=1) as source:
-                
-                speak("Please tell us your illness or condition... ")
-                print("Please tell us your illness or condition... ")
-                audio=r.listen(source)
-                print(audio)
-                try:    
-                    queryspeak = r.recognize_google(audio)
-                    return queryspeak                   
-                    break
-                except:
-                    print("Try Again")
-
-        
-    speak("Quick    and    easy    medical    solutions    by    quick doc") #Started
-    print("Would you like to type or speak? (T/S)")
-    mode=input(" ")
+def getQuery():
+    r = sr.Recognizer()
     while True:
-        if (mode=="T"):
-            print("1. View details of your illness")
-            print("2. View symptoms of your illness")
-            print("3. View preventive measures for your illness")
-            opt=int(input(" "))       
-            if opt==1:
-                qtext=input("Please enter your illness or condition: ")
-                qtext=qtext.lower()
-                print("\n")
-                print(wk.summary(qtext,5))
-                speak(wk.summary(qtext,5)) 
-            elif opt==2:
-                qtext=input("Please enter your illness or condition: ")      
-                qtext=qtext.lower()
-                pagepy=wiki_wiki.page(qtext)
-                symptoms = pagepy.section_by_title('Signs and symptoms') 
-                stringstring=symptoms.full_text().split('\n')
-                symp=' '.join(stringstring[1].split()[0:150])
-                print(symp)
-                print("\n")
-            elif opt==3:
-                qtext=input("Please enter your illness or condition: ")
-                pagepy=wiki_wiki.page(qtext)
-                symptoms = pagepy.section_by_title('Prevention') 
-                stringstring=symptoms.full_text().split('\n')
-                symp=' '.join(stringstring[1].split()[0:150])
-                print(symp)
-                print("\n")
-            elif opt==4:
-                break
-
-        
+        with sr.Microphone(device_index=1) as source:
             
-        elif (mode=="S"):
-            print("1. View details of your illness")
-            speak("View details of your illness")
-            print("2. View symptoms of your illness")
-            speak("View symptoms of your illness")
-            print("3. View preventive measures for your illness")
-            speak("View preventive measures for your illness")
-            tm.sleep(2)
-            speak("What would you like to do?")
-            opt=int(input(" "))       
-            if opt==1:
-                print("Please enter your illness or condition: ")
-                speak("Please enter your illness or condition: ")
-                qspeak=getQuery()
-                print("\n")
-                print(wk.summary(qspeak,5))
-                speak(wk.summary(qspeak,5)) 
-            elif opt==2:
-                print("Please enter your illness or condition: ") 
-                speak("Please enter your illness or condition: ")
-                qspeak=getQuery()    
-                pagepy=wiki_wiki.page(qspeak)
-                symptoms = pagepy.section_by_title('Signs and symptoms') 
-                stringstring=symptoms.full_text().split('\n')
-                symp=' '.join(stringstring[1].split()[0:150])
-                print(symp)
-                speak(symp)
-                print("\n")
-            elif opt==3:
-                print("Please enter your illness or condition: ")
-                speak("Please enter your illness or condition: ")
-                qspeak=getQuery()
-                pagepy=wiki_wiki.page(qspeak)
-                symptoms = pagepy.section_by_title('Prevention') 
-                stringstring=symptoms.full_text().split('\n')
-                symp=' '.join(stringstring[1].split()[0:150])
-                print(symp)
-                speak(symp)
-                print("\n")
-            elif opt==4:
+            speak("Please tell us your illness or condition... ")
+            print("Please tell us your illness or condition... ")
+            audio=r.listen(source)
+            try:    
+                queryspeak = r.recognize_google(audio)
+                return queryspeak                   
                 break
+            except:
+                print("Try Again")
 
-aiAssistant()  
+
+speak("Quick    and    easy    medical    solutions    by    quick doc") #Started
+print("Would you like to type or speak? (T/S)")
+mode=input(" ")
+while True:
+    if (mode=="T"):
+        print("1. View details of your illness")
+        print("2. View symptoms of your illness")
+        print("3. View preventive measures for your illness")
+        print("4. Exit")
+        opt=int(input(" "))       
+        if opt==1:
+            qtext=input("Please enter your illness or condition: ")
+            qtext=qtext.lower()
+            print("\n")
+            print(wk.summary(qtext,5))
+            speak(wk.summary(qtext,5)) 
+        elif opt==2:
+            qtext=input("Please enter your illness or condition: ")      
+            qtext=qtext.lower()
+            pagepy=wiki_wiki.page(qtext)
+            symptoms = pagepy.section_by_title('Signs and symptoms') 
+            stringstring=symptoms.full_text().split('\n')
+            symp=' '.join(stringstring[1].split()[0:150])
+            print(symp)
+            print("\n")
+        elif opt==3:
+            qtext=input("Please enter your illness or condition: ")
+            pagepy=wiki_wiki.page(qtext)
+            symptoms = pagepy.section_by_title('Prevention') 
+            stringstring=symptoms.full_text().split('\n')
+            symp=' '.join(stringstring[1].split()[0:150])
+            print(symp)
+            print("\n")
+        elif opt==4:
+            break
+
+
+
+    elif (mode=="S"):
+        print("1. View details of your illness")
+        speak("View details of your illness")
+        print("2. View symptoms of your illness")
+        speak("View symptoms of your illness")
+        print("3. View preventive measures for your illness")
+        speak("View preventive measures for your illness")
+        print("4. Exit")
+        speak("Exit")
+        tm.sleep(0.5)
+        speak("What would you like to do?")
+        opt=int(input(" "))       
+        if opt==1:
+            print("Please enter your illness or condition: ")
+            speak("Please enter your illness or condition: ")
+            qspeak=getQuery()
+            print("\n")
+            print(wk.summary(qspeak,5))
+            speak(wk.summary(qspeak,5)) 
+        elif opt==2:
+            print("Please enter your illness or condition: ") 
+            speak("Please enter your illness or condition: ")
+            qspeak=getQuery()    
+            pagepy=wiki_wiki.page(qspeak)
+            symptoms = pagepy.section_by_title('Signs and symptoms') 
+            stringstring=symptoms.full_text().split('\n')
+            symp=' '.join(stringstring[1].split()[0:150])
+            print(symp)
+            speak(symp)
+            print("\n")
+        elif opt==3:
+            print("Please enter your illness or condition: ")
+            speak("Please enter your illness or condition: ")
+            qspeak=getQuery()
+            pagepy=wiki_wiki.page(qspeak)
+            symptoms = pagepy.section_by_title('Prevention') 
+            stringstring=symptoms.full_text().split('\n')
+            symp=' '.join(stringstring[1].split()[0:150])
+            print(symp)
+            speak(symp)
+            print("\n")
+        elif opt==4:
+            break
